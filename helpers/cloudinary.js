84c30ec62eae529bdc39dc12ba4a519439b2d75a
Ulver <lorito.free@gmail.com>
2022-12-05 12:43:39 -0400

    getAllImagesByTags getImageByPublicId,,renameImageByPublicId,deleteImageById2,addTagsInImages,removeAllTags,removeTag,transformImage,transformImageQuality

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -21,9 +21,24 @@ const uploadImage = async (route) => {
   try {
     const result = await cloudinary.uploader.upload(route, {
       resource_type: 'image',
-      // folder: 'test/products',
+      folder: 'test/products',
       // public_id: 'gato',
       // transformation: [{ effect: 'background_removal' }],
+    })
+
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+const uploadImage2 = async (route) => {
+  try {
+    const result = await cloudinary.uploader.upload(route, {
+      resource_type: 'image',
+      folder: 'test/products',
+      // public_id: 'gato',
+      // transformation: [{ effect: 'background_removal' }],
+      categorization: 'imagga_tagging',
     })
 
     console.log(result)
@@ -84,12 +99,132 @@ const getAllImage = async () => {
   }
 }
 
+const getAllImagesByTags = async (tag) => {
+  try {
+    const result = await cloudinary.api.resources_by_tag(tag)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getImageByPublicId = async (prefix) => {
+  try {
+    const result = await cloudinary.api.resources({
+      type: 'upload',
+      prefix,
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const renameImageByPublicId = async (route, id) => {
+  try {
+    const result = await cloudinary.uploader.rename(route, id, {
+      overwrite: true,
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteImageById2 = async (id) => {
+  try {
+    const result = await cloudinary.api.delete_resources(...id)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const addTagsInImages = async (tag, routes) => {
+  try {
+    const result = await cloudinary.uploader.add_tag(tag, routes)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+const removeAllTags = async (ids) => {
+  try {
+    const result = await cloudinary.uploader.remove_all_tags(ids)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const removeTag = async (tag, ids) => {
+  try {
+    const result = await cloudinary.uploader.remove_tag(tag, ids)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const transformImage = async (id) => {
+  try {
+    const result = await cloudinary.url(id, {
+      transformation: { width: 300, height: 300, quality: 'auto' },
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+const transformImageQuality = async (id) => {
+  try {
+    const result = await cloudinary.url(id, {
+      transformation: { quality: 'auto' },
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
 // destroyImage('gato')
 // uploadImage('./assets/loro.jpg')
 // uploadImage('./assets/carpi.jpg')
 // uploadImage('./assets/hipo.webp')
+// uploadImage('./assets/sapo.jpg')
 
 // getUrl('./test/products/gato')
 
-// getImage('./test/products/gato')
-getAllImage()
+// getImage('./test/products/loro')
+// getAllImage()
+// getImageByPublicId('test/products/loro')
+
+// renameImageByPublicId(
+//   'test/products/kvdheehtbdmkg8ibrbkq',
+//   'test/products/loro'
+// )
+
+// deleteImageById2(['ahgqbtkpnd'])
+// addTagsInImages('tiernos', [
+//   'test/products/loro',
+//   'test/products/qjweiepygsgwyi7g1li1',
+//   'test/products/molskmjcajnwgbuxzbs9',
+//   'test/products/gato',
+// ])
+
+// getAllImagesByTags('gorditos')
+// removeAllTags([
+//   'test/products/loro',
+//   'test/products/qjweiepygsgwyi7g1li1',
+//   'test/products/molskmjcajnwgbuxzbs9',
+//   'test/products/gato',
+// ])
+
+// removeTag('gorditos', [
+//   'test/products/loro',
+//   'test/products/qjweiepygsgwyi7g1li1',
+//   'test/products/gato',
+// ])
+// uploadImage2('./assets/delfin.jpg')
+
+// transformImage('./test/products/loro')
+transformImageQuality('./test/products/loro')
